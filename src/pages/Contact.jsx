f7854ee617dc570c79484b87d00e4edb0ff69a9e
Hassan Javed <hassanjaved0554@gmail.com>
2025-12-15 19:24:04 +0500

    Completed contact page, added Wishlist page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -6,14 +6,15 @@ import Button from "../components/common/Button";
 import { PhoneIcon, EnvelopeIcon } from "@heroicons/react/24/outline";
 
 export default function Contact() {
+
     return (
         <>
             <TopHeader />
             <Header />
 
-            <section>
-                <div className="sm:w-4/5 mx-auto flex">
-                    <div className="text-sm w-2/5 p-6 space-y-4">
+            <section className="h-dvh flex justify-center items-center">
+                <div className="sm:w-4/5 mx-auto flex flex-col sm:flex-row max-sm:gap-8 sm:gap-2">
+                    <div className="text-sm sm:w-2/6 lg:w-1/4 p-2 sm:p-6 space-y-4 flex flex-col">
                         {/* contact details */}
                         <div className="flex items-center gap-4">
 
@@ -43,41 +44,60 @@ export default function Contact() {
 
                     </div>
 
-                    <div className="w-3/5 p-10 flex flex-col gap-8">
+                    <form className="sm:w-4/6 lg:w-3/4 p-2 sm:p-6 flex flex-col gap-8">
                         {/* contact form */}
-                        <div className="flex gap-4">
-                            <input
-                                type="text" 
-                                placeholder=""
-                                className="p-3 text-base bg-[#F5F5F5] outline-none flex-1"
-                            />
+                        <div className="flex flex-wrap gap-4">
 
-                            <input
-                                type="text" 
-                                placeholder=""
-                                className="p-3 text-base bg-[#F5F5F5] outline-none flex-1"
-                            />
+                            <div className="w-full flex-1">
+                                <input
+                                    type="text" 
+                                    placeholder="Your Name *"
+                                    required
+                                    className="peer w-full p-3 text-base focus:outline-black bg-[#F5F5F5] invalid:outline outline-red-600"
+                                />
+                                <p className="mt-1 hidden text-sm text-red-600 peer-invalid:block">
+                                    Name is required
+                                </p>
+                            </div>
 
-                            <input
-                                type="text" 
-                                placeholder=""
-                                className="p-3 text-base bg-[#F5F5F5] outline-none flex-1"
-                            />
+                            <div className="w-full flex-1">
+                                <input
+                                    type="email" 
+                                    placeholder="Your Email *"
+                                    required
+                                    className="peer w-full p-3 text-base focus:outline-black bg-[#F5F5F5] flex-1 invalid:outline outline-red-600"
+                                />
+                                <p className="mt-1 hidden text-sm text-red-600 peer-invalid:block">
+                                    Email is required
+                                </p>
+                            </div>
+
+                            <div className="w-full flex-1">
+                                <input
+                                    type="number" 
+                                    placeholder="Your Phone *"
+                                    required
+                                    className="peer w-full p-3 text-base focus:outline-black bg-[#F5F5F5] flex-1 invalid:outline outline-red-600"
+                                />
+                                <p className="mt-1 hidden text-sm text-red-600 peer-invalid:block">
+                                    Phone number is required
+                                </p>
+                            </div>
                         </div>
 
                         <div className="w-full">
-                            <input
-                                type="textarea" 
-                                placeholder=""
-                                className="p-3 text-base bg-[#F5F5F5] outline-none w-full"
-                            />
+                            <textarea
+                                rows={7}
+                                placeholder="Your Message"
+                                className="p-3 text-base focus:outline-black bg-[#F5F5F5] w-full resize-none"
+                            ></textarea>
                         </div>
 
-                        <div className="">
+                        <div className="flex justify-end-safe">
                             <Button text="Send Message" />
                         </div>
                         
-                    </div>
+                    </form>
                 </div>
             </section>
 
---
+++ src/pages/Wishlist.jsx
@@ -0,0 +1,14 @@
+import TopHeader from "../components/common/TopHeader";
+import Header from "../components/common/Header";
+import Footer from "../components/common/Footer";
+
+export default function Wishlist() {
+    return (
+        <>
+            <TopHeader />
+            <Header />
+
+            <Footer />
+        </>
+    )
+}
